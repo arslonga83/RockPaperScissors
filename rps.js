d4beda3329af4d51a4a467aec456ef6e0b739a99
arslonga83 <jlwood83@gmail.com>
2022-06-30 16:45:56 -0700

    write computerPlay function using Math.random

--- rps.js
+++ rps.js
@@ -1,1 +1,17 @@
-console.log('test')
+function computerPlay() {
+    let randomNum = (Math.floor(Math.random()*(3)+1));
+    if (randomNum === 1) {
+        console.log('Rock')
+    }
+    else if (randomNum === 2) {
+        console.log('Paper')
+    }
+    else if (randomNum === 3) {
+        console.log('Scissors')
+    }
+};
+
+computerPlay();
+
+
+
