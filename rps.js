6727c5d284d59e5496c7eb6581167201eef8fa6f
arslonga83 <jlwood83@gmail.com>
2022-07-01 12:16:32 -0700

    add code to report final count of wins/losses and result of 5 rounds. left comment code that was used for testing.

--- rps.js
+++ rps.js
@@ -21,40 +21,68 @@ function computerResult() {
 function playRound (playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === 'rock') {
         if (computerSelection === 3) {
-            console.log('You win! Rock breaks Scissors.');   
+             
+            let roundResult = 'win';
+            console.log('You win! Rock breaks Scissors.')
+         //   console.log(roundResult);
+            win = win + 1;
+         //   console.log(win)        
         }
         else if (computerSelection === 2) {
+            
+            let roundResult = 'loss';
             console.log('You lose. Paper covers Rock');
+         //   console.log(roundResult);
+            loss = loss + 1;
+         //   console.log(loss)     
         }
         else if (computerSelection === 1) {
             console.log('It\'s a tie!')
         }
     }
     else if (playerSelection.toLowerCase() === 'paper') {
-        if (computerSelection === 3) {
+        if (computerSelection === 3) {   
+            let roundResult = 'loss';
             console.log('You lose. Scissors cut paper.');
+        //    console.log(roundResult);
+            loss = loss + 1;
+        //    console.log(loss)   
         }
         else if (computerSelection === 2) {
-            console.log('It\'s a tie!')
+            console.log('It\'s a tie!');
         }
-        else if (computerSelection === 1) {
+        else if (computerSelection === 1) {    
+            let roundResult = 'win';
             console.log('You win! Paper covers Rock');
+         //   console.log(roundResult);
+            win = win + 1;
+         //   console.log(win)    
         }
     }
     else if (playerSelection.toLowerCase() === 'scissors') {
         if (computerSelection === 3) {
-            console.log('It\'s a tie!') 
+            console.log('It\'s a tie!');
         }
-        else if (computerSelection === 2) {
+        else if (computerSelection === 2) {    
+            let roundResult='win';
             console.log('You win! Scissors cut paper');
+         //   console.log(roundResult);
+            win = win + 1;
+         //   console.log(win)    
         }
         else if (computerSelection === 1) {
+            let roundResult='loss';
             console.log('You lose. Rock breaks scissors.');
-        }
+         //   console.log(roundResult);
+            loss = loss + 1;
+        //    console.log(loss)   
+        } 
     }
 }
 
-//now we play 5 rounds
+//this will play 5 rounds
+let win = 0;
+let loss = 0;
 function playGame () {
   for (let i = 0; i < 5; i++) {
     computerPlay();
@@ -65,7 +93,24 @@ function playGame () {
   }
 }
 
+//here's the final game which will report results
 playGame();
+console.log ('');
+console.log('Wins = ' + win + '. Losses = ' + loss + '.');
+if (win > loss) {
+    console.log('You won the game!')
+}
+else if (loss > win) {
+    console.log('Sorry, you lose the game.')
+}
+else if (win = loss) {
+    console.log('The game is a tie!')
+}
+
+
+
+
+
 
 
 
