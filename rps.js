c3033f20605053239b2989c728c151c000987a7b
arslonga83 <jlwood83@gmail.com>
2022-06-30 22:20:43 -0700

    setup loop to play 5 rounds

--- rps.js
+++ rps.js
@@ -2,21 +2,10 @@
 //Makes a random play choice for the computer
 function computerPlay() {
     return randomNum = (Math.floor(Math.random()*(3)+1));
-//MOVED THIS PART OUTSIDE FOR NOW....wasn't sure how to return random number outside otherwise
-  //  if (randomNum === 1) {
-   //     console.log('Computer plays Rock');
-  //  }
-   // else if (randomNum === 2) {
-    //    console.log('Computer plays Paper')
-   // }
-   // else if (randomNum === 3) {
-    //    console.log('Computer plays Scissors');
-  // }
-   
 };
 
-computerPlay();
-
+//Announces the computer play
+function computerResult() {
    if (randomNum === 1) {
         console.log('Computer plays Rock');
     }
@@ -26,20 +15,16 @@ computerPlay();
     else if (randomNum === 3) {
         console.log('Computer plays Scissors');
    }
-
-//for testing
-let playerSelection = prompt('Rock, Paper, or Scissors?')
-let computerSelection = randomNum
-//
+}
 
 //one play round - prints winners in console based on test selections above
 function playRound (playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === 'rock') {
         if (computerSelection === 3) {
-            console.log('You win! Rock breaks Scissors.')
+            console.log('You win! Rock breaks Scissors.');   
         }
         else if (computerSelection === 2) {
-            console.log('You lose. Paper covers Rock')
+            console.log('You lose. Paper covers Rock');
         }
         else if (computerSelection === 1) {
             console.log('It\'s a tie!')
@@ -47,13 +32,13 @@ function playRound (playerSelection, computerSelection) {
     }
     else if (playerSelection.toLowerCase() === 'paper') {
         if (computerSelection === 3) {
-            console.log('You lose. Scissors cut paper.')   
+            console.log('You lose. Scissors cut paper.');
         }
         else if (computerSelection === 2) {
             console.log('It\'s a tie!')
         }
         else if (computerSelection === 1) {
-            console.log('You win! Paper covers Rock')
+            console.log('You win! Paper covers Rock');
         }
     }
     else if (playerSelection.toLowerCase() === 'scissors') {
@@ -61,15 +46,28 @@ function playRound (playerSelection, computerSelection) {
             console.log('It\'s a tie!') 
         }
         else if (computerSelection === 2) {
-            console.log('You win! Scissors cut paper')
+            console.log('You win! Scissors cut paper');
         }
         else if (computerSelection === 1) {
-            console.log('You lose. Rock breaks scissors.')
+            console.log('You lose. Rock breaks scissors.');
         }
     }
 }
 
-playRound(playerSelection, computerSelection);
+//now we play 5 rounds
+function playGame () {
+  for (let i = 0; i < 5; i++) {
+    computerPlay();
+    let playerSelection = prompt('Rock, Paper, or Scissors?');
+    let computerSelection = randomNum;
+    computerResult();
+    playRound(playerSelection, computerSelection);
+  }
+}
+
+playGame();
+
+
 
 
 
