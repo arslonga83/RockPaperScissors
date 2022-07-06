5b3a4348909374be4e39d783edd526e0c003df09
arslonga83 <jlwood83@gmail.com>
2022-07-06 10:55:30 -0700

    update script for button input and output on screen. add styles page. add open source images.

--- rps.js
+++ rps.js
@@ -1,24 +1,39 @@
+//reference html elements
 const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
+const comp = document.querySelector('#comp');
+const human = document.querySelector('#human');
+const score = document.querySelector('#score');
 
+//set inital win count
+let win = 0;
+let loss = 0;
+
+//click events to control each turn
 rock.addEventListener('click', () => {
-    let playerSelection = 'rock';
-    console.log(playerSelection);
+    computerPlay();
+    let computerSelection = randomNum;
+    computerResult();
+    playRound('rock', computerSelection);
+    winCount()
 })
 
 paper.addEventListener('click', () => {
-    let playerSelection = 'paper';
-    console.log(playerSelection);
+    computerPlay();
+    let computerSelection = randomNum;
+    computerResult();
+    playRound('paper', computerSelection);
+    winCount();
 })
 
 scissors.addEventListener('click', () => {
-    let playerSelection = 'scissors';
-    console.log(playerSelection);
+    computerPlay();
+    let computerSelection = randomNum;
+    computerResult();
+    playRound('scissors', computerSelection);
+    winCount();
 })
-
-
-
 
 //Makes a random play choice for the computer
 function computerPlay() {
@@ -28,13 +43,13 @@ function computerPlay() {
 //Announces the computer play
 function computerResult() {
     if (randomNum === 1) {
-        console.log('Computer plays Rock');
+        comp.textContent = 'Computer plays Rock'
     }
     else if (randomNum === 2) {
-        console.log('Computer plays Paper')
+        comp.textContent = 'Computer plays Paper'
     }
     else if (randomNum === 3) {
-        console.log('Computer plays Scissors');
+        comp.textContent = 'Computer plays Scissors'
     }
 }
 
@@ -43,92 +58,70 @@ function playRound (playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === 'rock') {
         if (computerSelection === 3) {
             let roundResult = 'win';
-            console.log('You win! Rock breaks Scissors.')
+            human.textContent = 'You win! Rock breaks Scissors.'
             win = win + 1;       
         }
         else if (computerSelection === 2) {
             let roundResult = 'loss';
-            console.log('You lose. Paper covers Rock');
+            human.textContent = 'You win! Rock breaks Scissors'
             loss = loss + 1;     
         }
         else if (computerSelection === 1) {
-            console.log('It\'s a tie!')
+            human.textContent = 'It\'s a tie!'
         }
     }
     else if (playerSelection.toLowerCase() === 'paper') {
         if (computerSelection === 3) {   
             let roundResult = 'loss';
-            console.log('You lose. Scissors cut paper.');
+            human.textContent = 'You lose. Scissors cut paper.';
             loss = loss + 1;  
         }
         else if (computerSelection === 2) {
-            console.log('It\'s a tie!');
+            human.textContent = 'It\'s a tie!';
         }
         else if (computerSelection === 1) {    
             let roundResult = 'win';
-            console.log('You win! Paper covers Rock');
+            human.textContent = 'You win! Paper covers Rock';
             win = win + 1;   
         }
     }
     else if (playerSelection.toLowerCase() === 'scissors') {
         if (computerSelection === 3) {
-            console.log('It\'s a tie!');
+            human.textContent = 'It\'s a tie!';
         }
         else if (computerSelection === 2) {    
             let roundResult='win';
-            console.log('You win! Scissors cut paper');
+            human.textContent = 'You win! Scissors cut paper';
             win = win + 1;   
         }
         else if (computerSelection === 1) {
             let roundResult='loss';
-            console.log('You lose. Rock breaks scissors.');
+            human.textContent = 'You lose. Rock breaks scissors.';
             loss = loss + 1;  
         } 
     }
     else {
         let roundResult = 'loss';
-        console.log('Oops! You lose this round.');
+        human.textContent = 'Oops! You lose this round.';
         loss = loss + 1;
     }
 }
 
-//this will play 5 rounds
-//let win = 0;
-//let loss = 0;
-//function playGame () {
-//    for (let i = 0; i < 5; i++) {
-//        computerPlay();
-//        let playerSelection = prompt('Rock, Paper, or Scissors?');
-//        let computerSelection = randomNum;
-//        computerResult();
-//        playRound(playerSelection, computerSelection);
-//    }
-//}
-
-//here's the final game which will report results
-console.log('Let\'s play Rock, Paper, Scissors! Enter your selections at the prompt.')
-playGame();
-console.log ('');
-console.log('Wins = ' + win + '. Losses = ' + loss + '.');
-if (win > loss) {
-    console.log('You won the game!')
+//keeps tally for first to 5 wins
+function winCount() {
+    score.textContent = 'Wins = ' + win + '         Losses = ' + loss;
+    if (win===5) {
+        score.textContent = 'You won the game ' + win + ' to ' + loss + '! Refresh the page to play again.';
+        rock.disabled = true;
+        paper.disabled = true;
+        scissors.disabled = true;
+    
+    }
+    else if (loss === 5) {
+       score.textContent = 'You lost the game ' + loss + ' to ' + win + '. Refresh the page to play again.';
+       rock.disabled = true;
+       paper.disabled = true;
+       scissors.disabled = true;
+        
+    }
 }
-else if (loss > win) {
-    console.log('Sorry, you lose the game.')
-}
-else if (win = loss) {
-    console.log('The game is a tie!')
-}
-console.log('');
-console.log('Thanks for playing! Refresh the page to play again.')
-
-
-
-
-
-
-
-
-
-
-
